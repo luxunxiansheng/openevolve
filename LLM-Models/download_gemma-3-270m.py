bec9cd220ea75e30e96dac07b7de5047f7def7ce
ornot-4090 <ornot2009@gmail.com>
2025-08-18 04:26:53 +0000

    update

--- LLM-Models/download_gemma-3-270m.py
+++ LLM-Models/download_gemma-3-270m.py
@@ -1,10 +1,10 @@
 from huggingface_hub import snapshot_download
 
 # The ID of the model on Hugging Face Hub
-repo_id = "unsloth/gemma-3-270m-it-GGUF"
+repo_id = "unsloth/gemma-3-270m-it"
 
 # The local directory where you want to download the model
-local_folder = "./models/gemma-3-270m-it-GGUF"
+local_folder = "./models/gemma-3-270m-itF"
 
 # Download all files from the repository to the specified folder
 snapshot_download(repo_id, local_dir=local_folder)
--- LLM-Models/start_gemma-3-270-it.sh
+++ LLM-Models/start_gemma-3-270-it.sh
@@ -1,17 +1,9 @@
-#python -m vllm.entrypoints.openai.api_server \
-#  --model /workspaces/opencontext/LLM-Models/models/gemma-3-270m \
-#  --host 0.0.0.0 \
-#  --port 8006 \
-#  --served-model-name gemma-3-270m-it \
-#  --gpu-memory-utilization 0.1
-
-
-# quantized
 python -m vllm.entrypoints.openai.api_server \
-  --model "/workspaces/opencontext/LLM-Models/models/gemma-3-270m-it-GGUF" \
-  --tokenizer "/workspaces/opencontext/LLM-Models/models/gemma-3-270m" \
-  --dtype "auto" \
+  --model /workspaces/opencontext/LLM-Models/models/gemma-3-270m \
   --host 0.0.0.0 \
   --port 8006 \
   --served-model-name gemma-3-270m-it \
+  --gpu-memory-utilization 0.1
+
+
 

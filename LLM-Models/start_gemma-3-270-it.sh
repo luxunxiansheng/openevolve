1abf7d2448827090badaecc6c6a141af062460bb
ornot-4090 <ornot2009@gmail.com>
2025-08-18 00:35:23 +0000

    d

--- LLM-Models/start_gemma-3-270-it.sh
+++ LLM-Models/start_gemma-3-270-it.sh
@@ -9,6 +9,7 @@
 # quantized
 python -m vllm.entrypoints.openai.api_server \
   --model "/workspaces/opencontext/LLM-Models/models/gemma-3-270m-it-GGUF" \
+  --tokenizer "/workspaces/opencontext/LLM-Models/models/gemma-3-270m" \
   --dtype "auto" \
   --host 0.0.0.0 \
   --port 8006 \
